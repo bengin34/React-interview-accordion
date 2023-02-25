56e41c6c26601b93e5a8826742c05eeae677c8c4
bengin34 <becaglar3434@gmail.com>
2023-02-25 11:23:40 +0300

    folder structure is created

---
+++ src/components/InterviewAccord.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InterviewAccord = (props) => {
+    console.log(props)
+  return (
+    <div>
+   Interview Accord
+    </div>
+  )
+}
+
+export default InterviewAccord
